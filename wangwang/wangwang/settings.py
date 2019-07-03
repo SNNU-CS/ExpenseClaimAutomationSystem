31abe0bca7d7c5bff2806416d0d28b3ed30d540f
Qi Zhao <zhaoqi99@outlook.com>
2019-07-03 21:40:36 +0800

    feat: 🎸 create account app

--- wangwang/wangwang/settings.py
+++ wangwang/wangwang/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'account',
 ]
 GRAPHENE = {
     'SCHEMA': 'wangwang.schema.schema'  # Where your Graphene schema lives
@@ -125,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Using custom user model
+# https://docs.djangoproject.com/en/2.2/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
+AUTH_USER_MODEL = 'account.User'
