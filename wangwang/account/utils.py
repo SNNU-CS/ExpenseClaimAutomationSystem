8ea0a50277165d9dc5978159b79cd8d1d1102ebf
Qi Zhao <zhaoqi99@outlook.com>
2019-07-04 14:27:32 +0800

    feat: 🎸 add user and group model

---
+++ wangwang/account/utils.py
@@ -0,0 +1,8 @@
+from time import time
+
+
+def user_avatar_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/avatar/<username>/<time>.<jpg/png>
+    name = str(time()).split('.')[0]
+    suffix = filename.split('.')[-1]
+    return 'avatar/{0}/{1}.{2}'.format(instance.username, name, suffix)
