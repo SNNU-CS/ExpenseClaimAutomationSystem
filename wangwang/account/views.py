751101b894e2df8a911e420b44da2777000c4f9c
Qi Zhao <zhaoqi99@outlook.com>
2019-07-04 15:29:12 +0800

    fix: 🐛 quick fix ci

--- wangwang/account/views.py
+++ wangwang/account/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
-from django.views import View
+# from django.views import View
 
 
-class UserAuthView(View):
-    def __init__(self, *args):
-        super(UserAuthView, self).__init__(*args))
+# class UserAuthView(View):
+#     def __init__(self, *args):
+#         super(UserAuthView, self).__init__(*args))
