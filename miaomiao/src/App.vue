93d448d0244fc35edd356e122fbe1daaaa9e3ac7
ZhangQian217 <ZhangQian217@outlook.com>
2019-09-11 01:35:26 +0800

    feat: 🎸 add login form

--- miaomiao/src/App.vue
+++ miaomiao/src/App.vue
@@ -1,36 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <Login />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import Login from "./components/Login";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
+    Login
   },
   data: () => ({
     //
-  }),
+  })
 };
 </script>
