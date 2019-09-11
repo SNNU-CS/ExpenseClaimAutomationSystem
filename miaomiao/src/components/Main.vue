e10c619f78d0dafe2ca31f8ae48dcda68fcced01
Qi Zhao <zhaoqi99@outlook.com>
2019-09-12 00:51:13 +0800

    refactor: 💡 front end router config

---
+++ miaomiao/src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <div class="box">
+      <Menu></Menu>
+    </div>
+    <div class="content-view">
+      <transition name="move" mode="out-in">
+        <router-view></router-view>
+      </transition>
+    </div>
+  </v-app>
+</template>
+
+<script>
+import Menu from "./Menu.vue";
+export default {
+  name: "main",
+  data() {
+    return {};
+  },
+  components: {
+    Menu
+  }
+};
+</script>
--- miaomiao/src/router.js
+++ miaomiao/src/router.js
@@ -1,23 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+import Home from './views/Home'
+import Main from './components/Main'
+import Login from './components/Login'
+import DashBoard from './components/DashBoard'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect: '/main'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/main',
+      component: Main,
+      children: [
+        {
+          path: 'test',
+          component: Home
+        },
+        {
+          path: 'dashboard',
+          component: DashBoard
+        }
+      ]
+    },
+    {
+      path: '/login',
+      component: Login
     }
   ]
 })
