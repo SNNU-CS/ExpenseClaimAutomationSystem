b7ae5eb27ee109200ce23f8efaea311b25b3120b
Qi Zhao <zhaoqi99@outlook.com>
2019-11-24 23:19:46 +0800

    feat: 🎸 token auth backend

--- wangwang/wangwang/settings/dev.py
+++ wangwang/wangwang/settings/dev.py
@@ -6,7 +6,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dev',
         'USER': 'postgres',
-        'PASSWORD': '123456',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
--- wangwang/wangwang/wangwang.py
+++ wangwang/wangwang/wangwang.py
@@ -1,13 +1,43 @@
 from django.utils import timezone
-from rest_framework.authentication import BaseAuthentication
 from django.utils.deprecation import MiddlewareMixin
 from django.db import models
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import JsonResponse
 from .exceptions import BaseException, UnknownException
+from account.models import User, Token
+from django.conf import settings
+from .exceptions import AuthenticationFailed, InvalidToken
+
+
+class MyAuthentication:
+    def authenticate(self, request):
+        header = request.META.get('HTTP_AUTHORIZATION')
+        if header is None:
+            # Todo: another exception
+            raise AuthenticationFailed
+        raw_token = self.get_raw_token(header)
+        token = Token.objects.get(token=raw_token)
+        if not token.verify():
+            raise InvalidToken
+        token.refresf_exp()
+        request.user = token.user
+
+    def get_raw_token(self, header):
+        parts = header.split()
+        if len(parts) == 0 or len(parts) != 2:
+            raise AuthenticationFailed
+        return parts[1]
 
 
 class MyMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        user = request.user
+        if not user.is_anonymous and user.is_authenticated:
+            return None
+        if request.path in settings.AUTH_CONFIG.get('AUTH_EXCLUDE_PATH'):
+            return None
+        MyAuthentication().authenticate(request)
+
     def process_exception(self, request, exception):
         if not isinstance(exception, BaseException):
             return JsonResponse(UnknownException().as_dict(), status=UnknownException().get_http_code())
