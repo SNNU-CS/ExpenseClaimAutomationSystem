5be0c689aa2584955e7524a929855e0fec3aa78a
Qi Zhao <zhaoqi99@outlook.com>
2019-11-25 00:13:52 +0800

    test: 💍 fix user model test

--- wangwang/account/tests/test_models.py
+++ wangwang/account/tests/test_models.py
@@ -5,8 +5,8 @@ from account.models import User, Organization
 class UserTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        User.objects.create_user(username='test1', password='test1')
-        User.objects.create_user(username='test2', first_name='张', last_name='三')
+        User.objects.create(username='test1', password='test1')
+        User.objects.create(username='test2', first_name='张', last_name='三')
 
     def test_user_display(self):
         test_user = User.objects.get(username='test1')
@@ -19,8 +19,8 @@ class UserTestCase(TestCase):
 class OrganizationTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        Organization.objects.create(name='org1')
+        Organization.objects.create(org_name='org1')
 
     def test_organization_name(self):
-        test_org = Organization.objects.get(name='org1')
+        test_org = Organization.objects.get(org_name='org1')
         self.assertEqual(str(test_org), 'org1')
