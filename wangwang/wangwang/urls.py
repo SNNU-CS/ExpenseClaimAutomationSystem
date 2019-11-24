b438d52a75b793bf8e69187ba9cef65b24de02dc
Qi Zhao <zhaoqi99@outlook.com>
2019-11-24 19:26:00 +0800

    docs: ✏️ REST API docs

--- wangwang/wangwang/urls.py
+++ wangwang/wangwang/urls.py
@@ -16,5 +16,10 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
-
-urlpatterns = [path('admin/', admin.site.urls), path('api/account/', include('account.urls'))]
+from rest_framework.documentation import include_docs_urls
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/account/', include('account.urls')),
+    path('api/', include('rest_framework.urls')),
+    path('docs/', include_docs_urls(title='Wangwang REST API 文档')),
+]
