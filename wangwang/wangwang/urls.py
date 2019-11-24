cdf9125377e5a436781b8920acd97339de0f9884
Qi Zhao <zhaoqi99@outlook.com>
2019-11-24 23:25:36 +0800

    fix: 🐛 requirements file and format code

--- wangwang/wangwang/urls.py
+++ wangwang/wangwang/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from rest_framework.documentation import include_docs_urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/account/', include('account.urls')),
