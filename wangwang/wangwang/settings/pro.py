38e5dd03d4ecd7ae7b5c1d155c6c4ec48b47b9b9
Qi Zhao <zhaoqi99@outlook.com>
2019-11-25 23:43:08 +0800

    ci: 🎡 auto deploy

--- wangwang/wangwang/settings/pro.py
+++ wangwang/wangwang/settings/pro.py
@@ -1,1 +1,12 @@
 from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wangwang',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
