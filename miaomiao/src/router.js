d79d200921b3e0ed3b29abe95297aa750986f06b
Icey116 <Mxy116@outlook.com>
2020-01-15 20:00:23 +0800

    一个 404页面

--- miaomiao/src/router.js
+++ miaomiao/src/router.js
@@ -5,6 +5,7 @@ import Login from "./views/Login";
 import DashBoard from "./views/DashBoard";
 import User from "./views/account/User.vue";
 import Role from "./views/account/Role.vue";
+import error from "./views/error/404.vue";
 Vue.use(Router);
 
 export default new Router({
@@ -32,6 +33,10 @@ export default new Router({
     {
       path: "/login",
       component: Login
+    },
+    {
+      path:"/error/404",
+      component: error
     }
   ]
 });
