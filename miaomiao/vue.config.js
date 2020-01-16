9abf2f6365647f1930e472ae5d0fc312e753b430
Qi Zhao <zhaoqi99@outlook.com>
2020-01-16 21:58:19 +0800

    fix: 🐛 avatar show
    ✅ Closes: #90

--- miaomiao/vue.config.js
+++ miaomiao/vue.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-	devServer: {
-		proxy: {
-			"/api": {
-				target: "http://127.0.0.1:8000"
-			}
-		},
-		disableHostCheck: true
-	}
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000"
+      },
+      "/media": {
+        target: "http://127.0.0.1:8000"
+      }
+    },
+    disableHostCheck: true
+  }
 };
--- wangwang/account/utils.py
+++ wangwang/account/utils.py
@@ -2,7 +2,7 @@ from time import time
 
 
 def user_avatar_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/avatar/<username>/<time>.<jpg/png>
+    # file will be uploaded to MEDIA_ROOT/avatar/<id>/<time>.<jpg/png>
     name = str(time()).split('.')[0]
     suffix = filename.split('.')[-1]
-    return 'avatar/{0}/{1}.{2}'.format(instance.username, name, suffix)
+    return 'avatar/{0}/{1}.{2}'.format(instance.id, name, suffix)
