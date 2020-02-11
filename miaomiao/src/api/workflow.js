877c48709130a218da875a831545637bc1dd6a61
Icey116 <Mxy116@outlook.com>
2020-02-11 23:38:38 +0800

    工作流表格

--- miaomiao/src/api/workflow.js
+++ miaomiao/src/api/workflow.js
@@ -1,8 +1,14 @@
 import request from "@/utils/request";
 
-export function ListTicket() {
+export function ListWorkflow() {
   return request({
     url: "workflow/workflows/",
     method: "get"
+  });
+}
+export function DeleteWorkflow(id) {
+  return request({
+    url: "workflow/workflows/" + id + "/",
+    method: "delete"
   });
 }
