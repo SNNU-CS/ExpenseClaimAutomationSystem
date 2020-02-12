a77bfd3c43725b0d8620e99e186472fd71773e41
Qi Zhao <zhaoqi99@outlook.com>
2020-02-12 11:54:00 +0800

    feat: 🎸 workflow manage frontend
    ✅ Closes: #136

--- miaomiao/src/router/workflow.js
+++ miaomiao/src/router/workflow.js
@@ -1,8 +1,16 @@
- import Manage from "@/views/workflow/Manage.vue";
-
+import Manage from "@/views/workflow/Manage.vue";
+import NewWorkflow from "@/views/workflow/NewWorkflow.vue";
 export default [
   {
     path: "workflow/manage",
     component: Manage
+  },
+  {
+    path: "workflow/new",
+    component: NewWorkflow
+  },
+  {
+    path: "workflow/edit/:id",
+    component: NewWorkflow
   }
 ];
