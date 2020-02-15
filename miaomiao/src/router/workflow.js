a98ccd9d9035ce5f0bb012bc4e7a601eda05f22f
Icey116 <Mxy116@outlook.com>
2020-02-15 18:11:08 +0800

    流转配置

--- miaomiao/src/router/workflow.js
+++ miaomiao/src/router/workflow.js
@@ -1,6 +1,8 @@
 import Manage from "@/views/workflow/Manage.vue";
 import Workflow from "@/views/workflow/Workflow.vue";
 import NewState from "@/views/workflow/components/NewState.vue";
+import StateSet from "@/views/workflow/components/StateSet.vue";
+import EditState from "@/views/workflow/EditState.vue";
 import NewWorkflow from "@/views/workflow/NewWorkflow.vue";
 import FlowChart from "@/views/workflow/FlowChart.vue";
 export default [
@@ -9,7 +11,15 @@ export default [
     component: Workflow
   },
   {
-    path: "workflow/:id/state/new",
+    path: "workflow/edit/:id",
+    component: EditState
+  },
+  {
+    path: "workflow/StateSet",
+    component: StateSet
+  },
+  {
+    path: "workflow/:workflowId/state/new",
     component: NewState
   },
   {
